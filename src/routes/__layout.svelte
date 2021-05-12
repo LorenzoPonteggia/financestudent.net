984271eb4d4fb6b6a731f55cf701f3759f4f12f2
LorenzoPonteggia <83926649+LorenzoPonteggia@users.noreply.github.com>
2021-05-12 10:19:15 +0000

    Update __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,6 +4,19 @@
 	import Footer from "../lib/Footer.svelte";
 </script>
 
+<svelte:head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-173178499-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-173178499-1');
+	</script>
+	<script data-ad-client="ca-pub-3352760081213595" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</svelte:head>
+
 <Navbar/>
 
 <slot />
